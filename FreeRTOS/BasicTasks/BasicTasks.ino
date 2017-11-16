81f1d6f35c608df7c4db80e01ff87cc5e1541599
Ahmad Kamal Nasir <dringakn@gmail.com>
2017-11-16 10:07:54 +0500

    First Task Implemented: Blink internal LED at 2HZ

--- FreeRTOS/BasicTasks/BasicTasks.ino
+++ FreeRTOS/BasicTasks/BasicTasks.ino
@@ -4,9 +4,24 @@
  Author:	Ahmad kamal
 */
 
+#include <Arduino_FreeRTOS.h>
+
+TaskHandle_t tskhdlBlink;
+void taskBlink(void*){
+	pinMode(LED_BUILTIN, OUTPUT);
+	while (true)
+	{
+		digitalWrite(LED_BUILTIN, HIGH);
+		vTaskDelay(250 / portTICK_PERIOD_MS);
+		digitalWrite(LED_BUILTIN, LOW);
+		vTaskDelay(250 / portTICK_PERIOD_MS);
+	}
+}
+
 // the setup function runs once when you press reset or power the board
 void setup() {
-
+	xTaskCreate(taskBlink, "taskBlink", 128, NULL, 2, &tskhdlBlink);
+	Serial.begin(11500);
 }
 
 // the loop function runs over and over again until power down or reset
