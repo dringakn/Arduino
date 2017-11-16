538bf413184cf443b4ff857193b294d3cd622962
Ahmad Kamal Nasir <dringakn@gmail.com>
2017-11-16 09:49:11 +0500

    Empty Project

---
+++ FreeRTOS/BasicTasks/BasicTasks.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		BasicTasks.ino
+ Created:	11/16/2017 9:15:18 AM
+ Author:	Ahmad kamal
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
