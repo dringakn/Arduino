e8421871389d23381d3b8c8c2ae1bb671ebeb010
Ahmad Kamal Nasir <dringakn@gmail.com>
2017-11-17 07:04:06 +0500

    Queue Example Created

--- FreeRTOS/TaskPirorities/TaskPirorities.ino
+++ FreeRTOS/TaskPirorities/TaskPirorities.ino
@@ -4,39 +4,56 @@ Created:	11/16/2017 11:17:32 PM
 Author:	Ahmad kamal
 */
 #include <Arduino_FreeRTOS.h>
+#define configUSE_QUEUE_SETS 1
 
 TaskHandle_t tskhdlHighPriorityTask;
 void taskHighPriority(void* pvParam){
+	static unsigned long ulCtr = 0;
 	while (true)
 	{
-		Serial.println("H");
+		Serial.print("H:"); Serial.println(ulCtr++);
+		vTaskDelay(pdMS_TO_TICKS(1000));
 	}
 }
 
 TaskHandle_t tskhdlMediumPriorityTask;
 void taskMediumPriority(void* pvParam){
+	static unsigned long ulCtr = 0;
 	while (true)
 	{
-		Serial.println("M");
+		Serial.print("M:"); Serial.println(ulCtr++);
+		vTaskDelay(pdMS_TO_TICKS(2000));
 	}
 }
 
 TaskHandle_t tskhdlLowPriorityTask;
 void taskLowPriority(void* pvParam){
+	static unsigned long ulCtr = 0;
 	while (true)
 	{
-		Serial.println("L");
+		Serial.print("L:"); Serial.println(ulCtr++);
+		vTaskDelay(pdMS_TO_TICKS(2000));
 	}
 }
+
+/* Idle hook functions MUST be called vApplicationIdleHook(), take no parameters,
+and return void. */
+void vApplicationIdleHook(void)
+{
+	static unsigned long ulCtr = 0;
+	/* This hook function does nothing but increment a counter. */
+	Serial.println(ulCtr++);
+}
+
 // the setup function runs once when you press reset or power the board
 void setup() {
 	Serial.begin(115200);
-	xTaskCreate(taskHighPriority, "taskHighPriority", 128, NULL, configMAX_PRIORITIES, &tskhdlHighPriorityTask);	//Priority = 4
-	xTaskCreate(taskMediumPriority, "taskMediumPriority", 128, NULL, configTIMER_TASK_PRIORITY, &tskhdlMediumPriorityTask);//Priority = 3
-	xTaskCreate(taskLowPriority, "taskLowPriority", 128, NULL, configMAX_CO_ROUTINE_PRIORITIES, &tskhdlLowPriorityTask);//Priority = 2
+	xTaskCreate(taskHighPriority, "taskHighPriority", 128, NULL, 2, &tskhdlHighPriorityTask);		//Priority = 2
+	xTaskCreate(taskMediumPriority, "taskMediumPriority", 128, NULL, 2, &tskhdlMediumPriorityTask);	//Priority = 1
+	xTaskCreate(taskLowPriority, "taskLowPriority", 128, NULL, 1, &tskhdlLowPriorityTask);			//Priority = 0
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-
+	
 }
