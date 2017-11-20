7293ceab5bbbc393beb68bb9b545ab22a47bc9d6
Ahmad Kamal Nasir <dringakn@gmail.com>
2017-11-20 10:01:10 +0500

    Software Timer Example Updated

--- FreeRTOS/SoftwareTimer/SoftwareTimer.ino
+++ FreeRTOS/SoftwareTimer/SoftwareTimer.ino
@@ -2,32 +2,44 @@
  Name:		SoftwareTimer.ino
  Created:	11/19/2017 11:31:39 PM
  Author:	Ahmad kamal
-*/
+ */
 #include <Arduino_FreeRTOS.h>
 #include <timers.h>
 //portYIELD();
-TimerHandle_t tmrAutoReload=NULL, tmrOneShot = NULL;
+TimerHandle_t tmrAutoReload = NULL, tmrOneShot = NULL;
 
-static void timerCallbackOneShot(TimerHandle_t timer){
-	TickType_t xTimeNow = xTaskGetTickCount();
-	Serial.print("OneShot Timer Callback"); Serial.println(xTimeNow);
-}
-
-static void timerCallbackAutoReload(TimerHandle_t timer){
-	TickType_t xTimeNow = xTaskGetTickCount();
-	Serial.print("AutoReload Timer Callback"); Serial.println(xTimeNow);
+static void timerCallback(TimerHandle_t timer){
+	uint32_t timerID = (uint32_t)pvTimerGetTimerID(timer);
+	vTimerSetTimerID(timer, (void*)++timerID);
+	if (timer == tmrOneShot){
+		Serial.println("OneShot Timer Callback:");
+		xTimerReset(tmrOneShot, 0);
+	}
+	else if(timer == tmrAutoReload){
+		Serial.println("AutoReload Timer Callback:");
+	}
 }
 
 // the setup function runs once when you press reset or power the board
 void setup() {
 	Serial.begin(115200);
-	tmrAutoReload = xTimerCreate("tmrAutoReload", pdMS_TO_TICKS(1000), pdTRUE, 0, timerCallbackAutoReload);
-	tmrOneShot = xTimerCreate("tmrOneShot", pdMS_TO_TICKS(5000), pdFALSE, 0, timerCallbackOneShot);
-	if (tmrAutoReload != NULL) xTimerStart(tmrAutoReload, 0);
-	if (tmrOneShot != pdFALSE)xTimerStart(tmrOneShot, 0);
+	tmrAutoReload = xTimerCreate("tmrAutoReload", pdMS_TO_TICKS(2500), pdTRUE, 0, timerCallback);
+	if (tmrAutoReload != NULL){
+		if (xTimerStart(tmrAutoReload, 0))
+			Serial.println("AutoReload Timer Started Successfully");
+		else
+			Serial.println("AutoReload Timer Couldn't be started");
+	}
+	tmrOneShot = xTimerCreate("tmrOneShot", pdMS_TO_TICKS(5000), pdFALSE, 0, timerCallback);
+	if (tmrOneShot != NULL){
+		if (xTimerStart(tmrOneShot, 0))
+			Serial.println("OneShot Timer Started Successfully");
+		else
+			Serial.println("OneShot Timer Couldn't be started");
+	}
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-  
+
 }
