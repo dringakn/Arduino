5238e28ebc063b4e7a1e977a3f8cd7d3ba620ad2
Ahmad Kamal Nasir <dringakn@gmail.com>
2018-01-20 21:13:11 +0500

    Updated Bluetooth

--- Sensors/BluetoothTransciever/BluetoothTransciever.ino
+++ Sensors/BluetoothTransciever/BluetoothTransciever.ino
@@ -3,9 +3,13 @@
  Created:	1/20/2018 3:38:29 PM
  Author:	Dr. -Ing. Ahmad Kamal Nasir (dringakn@gmail.com, http://web.lums.edu.pk/~akn/)
  License:	This Library is licensed under a GPLv3 License
- Description: This program is used to change the settings of a 
-			Bluetooth transciever HC-05/HC-06 connected to a
-			ArduinoMega 2560 board.
+ Description: This program is used to change the settings of a Bluetooth transciever HC-05/HC-06 connected to a
+			ArduinoMega 2560 board. The Bluetooth module HC-05 is a MASTER/SLAVE module.The Role of the module 
+			(Master or Slave) can be configured only by AT COMMANDS. By default the factory setting is SLAVE.
+			Remember the models HC-04 or HC-06 are SLAVE only modules. To identify the model you can see the 
+			pin count.If the module has 5 or 6 pins it is HC-05.If the module has only 4 pins then it is 
+			HC-04 or HC-06. The slave modules can not initiate a connection to another Bluetooth device, 
+			but can accept connections.Master  module can initiate a connection to other devices. 
 			------------------------------
 			 ArduinoMega     HC-05/06
 			 ------------------------------
@@ -15,14 +19,16 @@
 			 Pin 3       --> STATUS
 			 ------------------------------
 			https://www.itead.cc/wiki/Serial_Port_Bluetooth_Module_(Master/Slave)_:_HC-05
+			http://cdn.instructables.com/ORIG/F3O/K70G/H1LWQ0PO/F3OK70GH1LWQ0PO.pdf
 */
 
 #define TERMINAL_BAUD 115200
-#define BLUETOOTH_DESIRED_BAUD 57600
+#define BLUETOOTH_DESIRED_BAUD 38400
+#define BLUETOOTH_DESIRED_MODE 1	// 1=Master 0=Slave 2=SlaveLoop
 #define BLUETOOTH_DESIRED_NAME "MYBT-1"
 #define BLUETOOTH_DESIRED_PIN "1234"
 
-const int KEY = 2;
+const int KEY = 2;	//KEY/EN
 const int STATUS = 3;
 const unsigned long BAUDS[] = { 1200,2400,4800,9600,19200,38400,57600,115200 };
 unsigned long baud;
@@ -38,18 +44,43 @@ void setup() {
 	getSettings();
 	setSettings();
 	Serial.println("<<< Commands Reference >>>");
-	Serial.println("AT+NAME : See default name");
-	Serial.println("AT+ADDR : see default address");
-	Serial.println("AT+VERSION : See version");
-	Serial.println("AT+UART : See baudrate");
-	Serial.println("AT+ROLE: See role of bt module(1=master/0=slave)");
-	Serial.println("AT+RESET : Reset and exit AT mode");
-	Serial.println("AT+ORGL : Restore factory settings");
-	Serial.println("AT+PSWD: see default password");
-	Serial.println("AT+RNAME: see remote name");
-	Serial.println("AT+CMODE: ");
-	Serial.println("AT+INQ: ");
+	Serial.println("AT Test UART Connection");
+	Serial.println("AT+RESET Reset Device");
+	Serial.println("AT+VERSION Querry firmware version");
+	Serial.println("AT+ORGL Restore settings to Factory Defaults");
+	Serial.println("AT+ADDR Query Device Bluetooth Address");
+	Serial.println("AT+RNAME Query Remote Bluetooth Device’s Name");
+	Serial.println("AT+INQ[ADD,PROFILE,RSSI] Query Nearby Discoverable Devices");
+	Serial.println("AT+INQC Cancel Search for Discoverable Devices");
+	Serial.println("AT+INQM=0/1,1-8,1-48 Query/Set Inquire Access Mode [standard/rssi,number of devices,timeout]");
+	Serial.println("AT+PAIR=add,timeout Device Pairing");
+	Serial.println("AT+ADCN Query Total Number of Device from Authenticated Device List");
+	Serial.println("AT+BIND=add Query/Set Binding Bluetooth Address");
+	Serial.println("AT+LINK=add Connect to a Remote Device");
+	Serial.println("AT+DISC Disconnect from a Remote Device");
+	Serial.println("AT+INIT Initialize SPP Profile");
+	Serial.println("AT+NAME=name Query/Set Device Name");
+	Serial.println("AT+ROLE=param Query/Set Device Role [0=Slave 1=Master 2=SlaveLoop]");
+	Serial.println("AT+CLASS=0 Query/Set Class of Device CoD");
+	Serial.println("AT+IAC=giac Query/Set Inquire Access Code");
+	Serial.println("AT+PSWD=pw Query/Set Pairing Passkey");
+	Serial.println("AT+UART=baud,stopbit,parity Query/Set UART parameter");
+	Serial.println("AT+CMODE=conn Query/Set Connection Mode [0=ConnectFixed 1=ConnectAny 2=SlaveLoop]");
+	Serial.println("AT+POLAR= Query/Set LED Output Polarity");
+	Serial.println("AT+PIO= Set/Reset a User I/O pin");
+	Serial.println("AT+MPIO= Query/Set/Reset multiple User I/O pin");
+	Serial.println("AT+IPSCAN Query/Set Scanning Parameters");
+	Serial.println("AT+SNIFF Query/Set SNIFF Energy Savings Parameters");
+	Serial.println("AT+SENM Query/Set Security & Encryption Modes");
+	Serial.println("AT+RMSAD Delete Authenticated Device from List");
+	Serial.println("AT+FSAD Find Device from Authenticated Device List");
+	Serial.println("AT+MRAD Query Most Recently Used Authenticated Device");
+	Serial.println("AT+STATE[INITIALIZED READY PAIRABLE PAIRED INQUIRING CONNECTING CONNECTED DISCONNECTED NUKNOW] Query Current Status of the Device");
+	Serial.println("AT+ENSNIFF Enter Energy Saving mode");
+	Serial.println("AT+EXSNIFF Exit Energy Saving mode");
 	Serial.println();
+	//at+link=11,22,987654
+	//at+rname?5472,4f,9912b2
 }
 
 void detectBaud() {
@@ -192,6 +223,9 @@ void setSettings() {
 
 	Serial1.print(String(String("AT+UART=") + String(BLUETOOTH_DESIRED_BAUD) + String(",0,0\r\n")));
 	Serial.print(Serial1.readString());
+
+	Serial1.print(String(String("AT+ROLE=") + String(BLUETOOTH_DESIRED_MODE) + String("\r\n")));
+	Serial.print(Serial1.readString());
 }
 
 void loop() {
@@ -199,4 +233,5 @@ void loop() {
 		Serial1.write(Serial.read());
 	if (Serial1.available())
 		Serial.write(Serial1.read());
+
 }
