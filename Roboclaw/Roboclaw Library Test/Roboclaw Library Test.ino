8e58acfc3a643632f85347c22d86fd1bc138cbb3
Ahmad Kamal Nasir <dringakn@gmail.com>
2018-11-02 13:12:33 +0500

    Roboclaw library test example

--- Roboclaw/Roboclaw Library Test/Roboclaw Library Test.ino
+++ Roboclaw/Roboclaw Library Test/Roboclaw Library Test.ino
@@ -3,35 +3,46 @@
  Created:	11/1/2018 6:26:50 PM
  Author:	Dr. -Ing. Ahmad Kamal Nasir
  Description:	This program is used to test the roboclaw communicaiton functionality using serial terminal.
-				Library requires to install SoftwareSerial Library for serial port communication.
-				The following library contains updated functionality of switch functions not present in the
-				original library
+				Roboclaw library requires SoftwareSerial library to be installed for serial port communication.
+				The library version contains updated functionality of switch functions which isnot present in the
+				original roboclaw library. This examples uses Serial1 of arduino mega, Pins TX1(18) and RX1(19), 
+				for roboclaw communication while Serial is used for terminal communication.
+Important Node: The S1 pin of the roboclaw is the recieve pin, therefore, to be connected to the TX pin of the 
+				arduino. The S2 pin of the roboclaw is the transmit pin, therfore, must be connected
+				to the RX pin of thr arduino. A pull-up resistor (1k) is required at the S2 pin of roboclaw.
 */
 #include "RoboClaw.h"
 
 RoboClaw roboclaw(&Serial1, 10000);
 
 void setup() {
-	Serial.begin(57600);	// Serial terminal baudrate.
-	roboclaw.begin(57600);	// Have you alrady changed the roboclaw baudrate through software app?
-}
-
-void serialEvent() {
-	// Read the serial terminal and forward the data to roboclaw
-	while (Serial.available())
-	{
-		Serial1.write(Serial.read());
-	}
-}
-
-void serialEvent1() {
-	// Read the roboclaw and forward the data to serial terminal
-	while (Serial1.available())
-	{
-		Serial.write(Serial1.read());
-	}
+	Serial.begin(38400);	// Have you changed the terminal baudrate?
+	roboclaw.begin(38400);	// Have you changed the roboclaw baudrate through software app?
+	pinMode(LED_BUILTIN, OUTPUT);	// Initialize on-board LED
 }
 
 void loop() {
+	while (Serial1.available())
+	{
+		// Read the roboclaw and forward the data to serial terminal
+		Serial.write(Serial1.read());
+	}
+	while (Serial.available())
+	{
+		// Read the serial terminal and forward the data to roboclaw
+		Serial1.write(Serial.read());
+	}
+	toggleLED(100);
+}
 
+void blinkLED(unsigned int ms) {
+	digitalWrite(LED_BUILTIN, HIGH);
+	delay(ms);
+	digitalWrite(LED_BUILTIN, LOW);
+	delay(ms);
+}
+
+void toggleLED(unsigned int ms) {
+	digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+	delay(ms);
 }
